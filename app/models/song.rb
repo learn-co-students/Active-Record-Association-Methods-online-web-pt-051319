0160d3de05d52c86ce94a3d96696ff7f77193b5f
James Britton <brittonjp@gmail.com>
2019-08-05 14:42:10 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,12 +7,17 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    unless Artist.all.map { |artist| artist.name }.include?("Drake")
-      drake = Artist.create(name: "Drake")
-    else
-      drake = Artist.all.find_by(name: "Drake")
-    end
+    
+    drake = Artist.find_or_create_by(name: "Drake")
+    
+    # unless Artist.all.map { |artist| artist.name }.include?("Drake")
+    #   drake = Artist.create(name: "Drake")
+    # else
+    #   drake = Artist.all.find_by(name: "Drake")
+    # end
+    
     self.artist = drake
+    
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
